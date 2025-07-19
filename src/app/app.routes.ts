c9f0fbf0da1a481012b5bbdc67045518b62b8c5d
seif <dr.saifos2001@gmail.com>
2025-07-19 18:03:41 +0300

    routing done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { Blank } from './layouts/blank/blank';
+import { Home } from './pages/home/home';
+import { Auth } from './layouts/auth/auth';
+import { Login } from './pages/login/login';
+import { Signup } from './pages/signup/signup';
+import { NotFound } from './pages/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: '',
+    component: Auth,
+    children: [
+      { path: 'login', component: Login, title: 'Nova - login' },
+      { path: 'signup', component: Signup, title: 'Nova - signup' },
+    ],
+  },
+  {
+    path: '',
+    component: Blank,
+    children: [{ path: 'home', component: Home, title: 'Nove - home' }],
+  },
+  { path: '**', component: NotFound, title: 'Not found' },
+];
---
+++ src/app/layouts/blank/blank.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-blank',
+  imports: [RouterOutlet],
+  templateUrl: './blank.html',
+  styleUrl: './blank.css',
+})
+export class Blank {}
