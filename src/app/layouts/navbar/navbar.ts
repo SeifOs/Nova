b0e5a5d65a26ab35790b3b24c145c38916f26b0f
Seif <dr.saifos2001@gmail.com>
2025-08-02 17:48:07 +0300

    dark mode to local storage

--- src/app/layouts/navbar/navbar.ts
+++ src/app/layouts/navbar/navbar.ts
@@ -1,3 +1,4 @@
+import { IsBrowser } from './../../core/services/isBrowser/is-browser';
 import { Component, inject, input, OnInit } from '@angular/core';
 import { initFlowbite } from 'flowbite';
 import { Flowbite } from '../../core/services/flowbite/flowbite';
@@ -17,17 +18,34 @@ export class Navbar implements OnInit {
   private readonly flowbiteService = inject(Flowbite);
   private readonly router = inject(Router);
   private readonly userData = inject(UserData);
+  private readonly isBrowser = inject(IsBrowser);
 
   isLogged = input<boolean>(false);
+  isDarkMode: boolean = false;
 
   ngOnInit(): void {
+    if (this.isBrowser.isBrowser()) {
+      if (localStorage.getItem('isDarkMode') != null) {
+        this.isDarkMode = localStorage.getItem('isDarkMode') == 'true';
+        if (this.isDarkMode) {
+          document.documentElement.classList.add('dark');
+        }
+      }
+    }
+
     this.flowbiteService.loadFlowbite((flowbite) => {
       initFlowbite();
     });
   }
 
   toggleDarkMode() {
-    document.documentElement.classList.toggle('dark');
+    if (!this.isDarkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+    this.isDarkMode = !this.isDarkMode;
+    localStorage.setItem('isDarkMode', this.isDarkMode.toString());
   }
 
   signOut() {
